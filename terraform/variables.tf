7f989061fe07405109719866c8775394c6d142d6
dnogu <dnogueira85@gmail.com>
2022-07-12 15:13:37 -0400

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,13 @@ variable "cloudflare_api_key" {
     description = "apikey"
     type = string
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+    description = "aws access key id"
+    type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    description = "aws access key"
+    type = string
+}
