9c848097ce9bb3f9e363b2c96a9f582c2a3b97f4
dnogu <dnogueira85@gmail.com>
2022-07-12 12:57:51 -0400

    update_region

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -13,3 +13,23 @@ data "aws_acm_certificate" "issued" {
   statuses = ["ISSUED"]
   most_recent = true
 }
+
+resource "aws_apigatewayv2_api" "mtig" {
+  name          = "mtig-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "mtig" {
+  api_id           = aws_apigatewayv2_api.mtig.id
+  integration_type = "HTTP"
+
+  description               = "Lambda example"
+  integration_method        = "GET"
+  integration_uri           = aws_lambda_function.dns_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_integration_response" "mtig" {
+  api_id                   = aws_apigatewayv2_api.mtig.id
+  integration_id           = aws_apigatewayv2_integration.mtig.id
+  integration_response_key = "/200/"
+}
