4afdc3703707b574520a4f5280dc7730aa4bb43e
dnogu <dnogueira85@gmail.com>
2022-07-12 15:30:16 -0400

    update

--- terraform/cloudflare_dns.tf
+++ terraform/cloudflare_dns.tf
@@ -5,7 +5,7 @@ data "cloudflare_zone" "example" {
 resource "cloudflare_record" "foobar" {
   zone_id = data.cloudflare_zone.example.id
   name    = var.env == "prod" ? "api" : "${var.env}.api"
-  value   = aws_apigatewayv2_api.mtig.api_endpoint
+  value   = aws_apigatewayv2_domain_name.mtig.domain_name_configuration[0].target_domain_name
   type    = "CNAME"
   proxied = true
 }
