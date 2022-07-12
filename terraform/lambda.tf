30be7f4c3c2f92b83c7744953f2d6462c8fed878
dnogu <dnogueira85@gmail.com>
2022-07-12 16:50:20 -0400

    update

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -65,7 +65,7 @@ resource "aws_iam_policy" "policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = aws_iam_role.iam_for_lambda.arn
+  role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.policy.arn
 }
 
