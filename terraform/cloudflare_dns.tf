de32366c30bea4856668ba9d23498c8e81007a3a
dnogu <dnogueira85@gmail.com>
2022-07-12 15:31:52 -0400

    update

--- terraform/cloudflare_dns.tf
+++ terraform/cloudflare_dns.tf
@@ -2,7 +2,7 @@ data "cloudflare_zone" "example" {
   name = "maketheinternetgo.com"
 }
 
-resource "cloudflare_record" "foobar" {
+resource "cloudflare_record" "mtig-api" {
   zone_id = data.cloudflare_zone.example.id
   name    = var.env == "prod" ? "api" : "${var.env}.api"
   value   = aws_apigatewayv2_domain_name.mtig.domain_name_configuration[0].target_domain_name
