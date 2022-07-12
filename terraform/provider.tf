ba284d370e2e386a824bcf9c2ee9319004cedabe
dnogu <dnogueira85@gmail.com>
2022-07-12 12:31:30 -0400

    update_region

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,9 +4,17 @@ terraform {
       source = "hashicorp/aws"
       version = "4.22.0"
     }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.2.0"
+    }
   }
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
+}
+
+provider "archive" {
+  # Configuration options
 }
